37f642ffdd8a6062a6a51eed51b84ac11d9ca279
Cabanillas Dulanto <ucabanillas@intercorp.com.pe>
2021-06-11 10:54:44 -0500

    Primer cambio

--- src/com/interbank/pe/Main.java
+++ src/com/interbank/pe/Main.java
@@ -3,6 +3,10 @@ package com.interbank.pe;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hola Canales Aletrnativos");
+
+        if (args.length < 1) {
+            System.err.println("Ingresar un valor");
+        }
+        System.out.println(args.length);
     }
 }
